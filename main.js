76deddd638a4803d00d7fc3ec3dd6cffd278fd8c
unknown <nguyentienquyet525@gmail.com>
2021-10-07 22:15:45 +0700

    upload source code

---
+++ main.js
@@ -0,0 +1,330 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// Fade in show
+const popUpFront = $('.popup-front');
+const closePopUp = $('.close-popup');
+const imgclosePopUp = $('.img-close-popup');
+closePopUp.onclick = function () {
+    popUpFront.style.display = 'none';
+}
+
+popUpFront.onclick = function () {
+    popUpFront.style.display = 'none';
+}
+
+imgclosePopUp.onclick = function (event) {
+    event.stopPropagation();
+}
+
+//Header and to header
+const header = $('#header');
+const clickToHeader = $('.click-to-header');
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        header.classList.add('header-fix');
+        clickToHeader.style.display = 'block';
+    } else {
+        header.classList.remove('header-fix');
+        clickToHeader.style.display = 'none';
+    }
+}
+
+//Header-right open
+const navIconRegion = $('#nav-icon-region');
+const menuRight = $('.menu-position-right');
+
+navIconRegion.onclick =function() {
+    if(this.classList.contains('open')) {
+        this.classList.remove('open');
+        menuRight.style.transform = 'translateX(100%)';
+    }else {
+        this.classList.add('open');
+        menuRight.style.transform = 'translateX(0)';
+    }
+}
+
+// Mobile menu full
+const menuToggleBtn = $('.menu-toggle-btn');
+const mobileMenuFull = $('.mobile-menu-full');
+menuToggleBtn.onclick = function() {
+    if(this.classList.contains('active')) {
+        this.classList.remove('active');
+        mobileMenuFull.style.transform = 'translateX(-100%)';
+    }else {
+        this.classList.add('active');
+        mobileMenuFull.style.transform = 'translateX(0)';
+    }
+}
+
+//Nav-item active
+const navItems = $$('.nav-item');
+const tabContents = $$('.tab-content');
+
+navItems.forEach(function(navItem, index) {
+
+    const tabContent = tabContents[index];
+    navItem.onclick = function(e) {
+        e.preventDefault();
+
+        $('.nav-item.active').classList.remove('active');
+        $('.tab-content.active').classList.remove('active');
+
+        this.classList.add('active');
+        tabContent.classList.add('active');
+    }
+})
+
+// Slide-img
+var slideIndex = 0;
+var i = 0;
+var slides = $$(".slide-img");
+
+//Clear all images
+function reset() {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+}
+
+showItemsBlock1();
+
+// Show images
+function showItemsBlock1() {
+    reset();
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showItemsBlock1, 4000); // Change image every 4 seconds
+}
+
+// Click prev
+$('#slide-pre').onclick = function () {
+    reset();
+    slideIndex--;
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Click next
+$('#slide-next').onclick = function () {
+    reset();
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+// Block -slide-container
+const blockNumbers = $$('.block-number');
+const blockSlideContainers = $$('.block-slide-container');
+
+blockNumbers.forEach(function(blockNumber, index) {
+
+    const blockSlideContainer = blockSlideContainers[index];
+    blockNumber.onclick = function(e) {
+
+        $('.block-number.active').classList.remove('active');
+        $('.block-slide-container.active').classList.remove('active');
+
+        this.classList.add('active');
+        blockSlideContainer.classList.add('active');
+    }
+})
+
+// Slide-item-block1
+var slideQuote = 0;
+var i = 0;
+
+var slideItems = $$(".swiper-slide .item");
+const pieCountDowns = $$('.pie-count-down');
+pieCountDowns.forEach(function(pieCountDown, index) {
+
+    const slideItem = slideItems[index];
+    pieCountDown.onclick = function() {
+
+        $('.pie-count-down.active').classList.remove('active');
+        $('.swiper-slide .item-active').classList.remove('active');
+
+        this.classList.add('active');
+        slideItem.classList.add('active');
+    }
+})
+
+// Slide-item-block2
+const swiperWrapper2 = $(".block2 .swiper-wrapper");
+const swiperSlides2 = $$(".block2 .swiper-slide");
+const pieCountDowns2 = $$(".block2 .swiper-pagination .pie-countdown");
+const widthSlide2 = swiperSlides2[2].offsetWidth;
+var next = widthSlide2 * 3/2;
+if (window.innerWidth < 768) {
+    next = widthSlide2 * 2;
+}
+let index = 2;
+let pageIndex = 0;
+swiperWrapper2.style.transform = 'translateX(-' + next + 'px)';
+setInterval(function() {
+    $(".block2 .swiper-slide.swiper-slide-active").classList.remove('swiper-slide-active');
+    $(".block2 .swiper-slide.swiper-slide-prev").classList.remove('swiper-slide-prev');
+    $(".block2 .swiper-slide.swiper-slide-next").classList.remove('swiper-slide-next');
+    $(".block2 .swiper-pagination .pie-countdown-swiper-active").classList.remove('pie-countdown-swiper-active');
+    if (window.innerWidth < 768) {
+        if(next < widthSlide2 * 4) {
+            next += widthSlide2;
+            if(index < 5) {
+                index++;
+            }
+            if(pageIndex < 3) {
+                pageIndex++;
+            }
+        } else {
+            next = widthSlide2 * 3/2;
+            if (window.innerWidth < 768) {
+                next = widthSlide2 * 2;
+            }
+            index = 2;
+            pageIndex = 0;
+        }
+    } else {
+        if(next < widthSlide2 * 3) {
+            next += widthSlide2;
+            if(index < 5) {
+                index++;
+            }
+            if(pageIndex < 3) {
+                pageIndex++;
+            }
+        } else {
+            next = widthSlide2 * 3/2;
+            if (window.innerWidth < 768) {
+                next = widthSlide2 * 2;
+            }
+            index = 2;
+            pageIndex = 0;
+        }
+    }
+    swiperWrapper2.style.transform = 'translateX(-' + next + 'px)';
+    swiperWrapper2.style.transition = 'all 0.3s linear';
+    swiperSlides2[index].classList.add('swiper-slide-active');
+    swiperSlides2[index - 1].classList.add('swiper-slide-prev');
+    swiperSlides2[index + 1].classList.add('swiper-slide-next');
+    pieCountDowns2[pageIndex].classList.add('pie-countdown-swiper-active');
+    let pieCountDownNotActives2 = $$(".block2 .swiper-pagination .pie-countdown:not(.pie-countdown-swiper-active)");
+    for(let i = 0; i < pieCountDownNotActives2.length; i++) {
+        pieCountDownNotActives2[i].innerHTML = `<span></span>`;
+    }
+    pieCountDowns2[pageIndex].innerHTML = `<div class="wrapper-circle" data-anim="base wrapper">
+                                                <div class="circle" data-anim="base left"></div>
+                                                <div class="circle" data-anim="base right"></div>
+                                            </div>
+                                            <label class="dot"></label>`;
+}, 5000)
+
+// Xe ô tô Block 3
+const swiperContainer3 = $(".block3 .swiper-container");
+const swiperWrapper3 = $(".block3 .swiper-wrapper");
+const swiperSlides3 = $$(".block3 .swiper-slide");
+const pieCountDowns3 = $$(".block3 .swiper-pagination .pie-countdown");
+
+const widthSlide3 = swiperSlides3[1].offsetWidth;
+let next3 = window.innerWidth;
+swiperSlides3[0].classList.add("swiper-slide-active")
+let index3 = 0;
+setInterval(function() {
+    $(".block3 .swiper-slide-active").classList.remove("swiper-slide-active")
+    $(".block3 .swiper-pagination .pie-countdown-swiper-active").classList.remove('pie-countdown-swiper-active');
+    if(index3 < 1) {
+        index3++;
+        next3 = widthSlide3;
+        console.log(index3);
+    } else {
+        index3 = 0;
+        next3 = 0;
+    }
+    swiperWrapper3.style.transform = "translate(-" + next3 + "px)";
+    swiperWrapper3.style.transition = "all 0.3s linear";
+    swiperSlides3[index3].classList.add("swiper-slide-active");
+    pieCountDowns3[index3].classList.add('pie-countdown-swiper-active');
+    let pieCountDownNotActives3 = $$(".block3 .swiper-pagination .pie-countdown:not(.pie-countdown-swiper-active)");
+    for(let i = 0; i < pieCountDownNotActives3.length; i++) {
+        pieCountDownNotActives3[i].innerHTML = `<span></span>`;
+    }
+    pieCountDowns3[index3].innerHTML = `<div class="wrapper-circle" data-anim="base wrapper">
+                                                <div class="circle" data-anim="base left"></div>
+                                                <div class="circle" data-anim="base right"></div>
+                                            </div>
+                                            <label class="dot"></label>`;
+}, 5000);
+
+// Xe máy Block 4
+const swiperContainer4 = $(".block4 .swiper-container");
+const swiperWrapper4 = $(".block4 .swiper-wrapper");
+const swiperSlides4 = $$(".block4 .swiper-slide");
+const pieCountDowns4 = $$(".block4 .swiper-pagination .pie-countdown");
+
+const widthSlide4 = swiperSlides4[1].offsetWidth;
+swiperSlides4[0].classList.add("swiper-slide-active")
+let index4 = 0;
+let next4 = widthSlide4;
+setInterval(function() {
+    $(".block4 .swiper-slide-active").classList.remove("swiper-slide-active")
+    $(".block4 .swiper-pagination .pie-countdown-swiper-active").classList.remove('pie-countdown-swiper-active');
+    if(index4 < 1) {
+        index4++;
+        next4 = widthSlide4;
+        console.log(index4);
+    } else {
+        index4 = 0;
+        next4 = 0;
+    }
+    swiperWrapper4.style.transform = "translate(-" + next4 + "px)";
+    swiperWrapper4.style.transition = "all 0.3s linear";
+    swiperSlides4[index4].classList.add("swiper-slide-active");
+    pieCountDowns4[index4].classList.add('pie-countdown-swiper-active');
+    let pieCountDownNotActives4 = $$(".block4 .swiper-pagination .pie-countdown:not(.pie-countdown-swiper-active)");
+    for(let i = 0; i < pieCountDownNotActives4.length; i++) {
+        pieCountDownNotActives4[i].innerHTML = `<span></span>`;
+    }
+    pieCountDowns4[index4].innerHTML = `<div class="wrapper-circle" data-anim="base wrapper">
+                                                <div class="circle" data-anim="base left"></div>
+                                                <div class="circle" data-anim="base right"></div>
+                                            </div>
+                                            <label class="dot"></label>`;
+}, 5000);
+
+
+
+
+// Responsive footer
+
+const listmenuFooters = $$(".list-menu")
+const menuFooters = $$(".list-menu .menu")
+const caretDowns = $$(".caret-down")
+const menuTitles = $$(".menu-title")
+menuTitles.forEach(function (menuTitle, index) {
+
+    const menuFooter = menuFooters[index];
+    const caretDown = caretDowns[index];
+    const listmenuFooter = listmenuFooters[index];
+    menuTitle.onclick = function () {
+        listmenuFooter.classList.toggle("active");
+        caretDown.classList.toggle('active');
+        // menuFooter.style.transition = 'all 0.3s linear';
+    }
+})
+
+
+
+
+
+
+
+
+
+
---
+++ render.js
@@ -0,0 +1,171 @@
+const app = {
+    cars: [
+        {
+            dongxe: "DÒNG SUV",
+            slogan: "CÙNG BẠN BỨT PHÁ" + "<br>" + "MỌI GIỚI HẠN",
+            name: "VF e34",
+            descs: ['Ô tô điện thông minh khởi tạo tương lai di chuyển xanh' , 
+                'Hệ thống pin tiên tiến, chính sách thuê pin độc đáo' , 
+                'Chi phí vận hành, bảo dưỡng rẻ hơn xe xăng'
+            ],
+            logo: './assets/img/vinfast-data-01/bg-title-car.png',
+            img: "./assets/img/vinfast-data-01/VFe34_1624348575.webp"
+        },
+        {
+            dongxe: "DÒNG SUV",
+            slogan: "CHINH PHỤC MỌI " + "<br>" + "CON ĐƯỜNG",
+            name: "LUX SA2.0",
+            descs: ['Thiết kế sang trọng, mạnh mẽ' , 
+                'Hệ dẫn động 4 bánh toàn thời gian cho cảm giác lái đầm chắc mà êm ái' , 
+                'Khung gầm liền khối vững chắc chuẩn châu Âu tối ưu' ,
+                'Chuẩn an toàn cao nhất 5 sao ASEAB NCAP'
+            ],
+            logo: './assets/img/vinfast-data-01/bg-title-car.png',
+            img: "./assets/img/vinfast-data-01/LuxSA_1624348590.webp"
+        },
+        {
+            dongxe: "DÒNG SEDAN",
+            slogan: "TẬN HƯỞNG TỪNG" + "<br>" + " KHOẢNH KHẮC",
+            name: "LUX A2.0",
+            descs: ['Thiết hế sang trọng, tính tế' , 
+                'Động cơ tăng áp mạnh mẽ' , 
+                'Hệ dẫn động cầu sau và hệ thống treo độc lâp cho cảm giác lái thể thao' ,
+                'Chuẩn an toàn cao nhất 5 sao ASEAB NCAP'
+            ],
+            logo: './assets/img/vinfast-data-01/bg-title-car.png',
+            img: "./assets/img/vinfast-data-01/LuxA_1624348606.webp"
+        },
+        {
+            dongxe: "DÒNG HATCHBACK",
+            slogan: "TỐI ƯU " + "<br>" + "MỌI TRẢI NGHIỆM",
+            name: "FADIL",
+            descs: ['Xe đô thị cỡ nhỏ hoàn hảo' , 
+                'Khả năng vận hành mạnh mẽ dẫn đầu phân khúc' , 
+                'Hệ thống giải trí và kết nối hiện đại' ,
+                'Chuẩn an toàn cao nhất 4 sao ASEAB NCAP'
+            ],
+            logo: './assets/img/vinfast-data-01/bg-title-car.png',
+            img: "./assets/img/vinfast-data-01/Fadil_1624348615.webp"
+        },
+        {
+            dongxe: "DÒNG SUV",
+            slogan: "DẤU ẤN " + "<br>" + "NGƯỜI THỦ LĨNH",
+            name: "PRESIDENT",
+            descs: ['Lời khẳng định về năng lực sản xuất ô tô của VinFast' , 
+                'Phiên bản giới hạn 500 chiếc trên toàn thế giới' , 
+                'Thiết kế uy lực đầy kiêu hãnh, tôn dấu ấn tinh hoa của chủ nhân' ,
+                'Khả năng vận hành vượt trội với động cơ V8 6,2L mạnh mẽ bậc nhất thế giới'
+            ],
+            logo: './assets/img/vinfast-data-01/bg-title-car.png',
+            img: "./assets/img/vinfast-data-01/president.webp"
+        }
+    ],
+    motorbikes: [
+        {
+            img: "./assets/img/vinfast-data-01/Theon_1624348475.webp",
+            name: "THEON",
+            slogan: "CÙNG BẠN BỨT PHÁ" + "<br>" + "TRẢI NGHIỆM ĐỈNH CAO",
+            descs: ['Vận hành mạnh mẽ, vận tốc lên tới 90 km/h' , 
+                'Hệ thống phanh ABS 2 kênh, chuẩn chống nước IP67' , 
+                'Công nghệ bứt phá với hệ thống PAAK, smart key, tích hợp eSIM' ,
+                'Công nghệ pin Lithium tiên tiến'
+            ]
+        },
+        {
+            img: "./assets/img/vinfast-data-01/Feliz_1624348483.webp",
+            name: "FELIZ",
+            slogan: "SỐNG XANH THANH LỊCH",
+            descs: ['Thiết kế thời thượng' , 
+                'Vận hành êm ái, hai chế độ lái Eco và Sport linh hoạt' , 
+                'Vận tốc tối đa 60 km/h, quãng đường lên tới 90 km' ,
+                'Cốp xe rộng tới 20 lít, tiêu chuẩn chống nước IP67'
+            ]
+        },
+        {
+            img: "./assets/img/vinfast-data-01/KlaraS_1624348494.webp",
+            name: "KLARA S",
+            slogan: "KHỞI ĐẦU PHONG CÁCH MỚI",
+            descs: ['Thiết kế sang trọng, tôn vinh vóc dáng Việt' , 
+                'Vận hành mạnh mẽ, quãng đường di chuyển tới 120 km' , 
+                'Công nghệ thông minh tích hợp eSIM' ,
+                'Tiêu chuẩn chống nước IP57'
+            ]
+        },
+        {
+            img: "./assets/img/vinfast-data-01/Impes_1624348519.webp",
+            name: "IMPES",
+            slogan: "TOẢ CHẤT RIÊNG - BẬT CÁ TÍNH",
+            descs: ['Thiết kế thể thao và cá tính' , 
+                'Tính năng thông minh vượt trội' , 
+                'Vận hành mạnh mẽ, vận tốc lên tới 49 km/h' ,
+                'Công nghệ pin Lithium bền bỉ, tiêu chuẩn chống nước IP57'
+            ]
+        },
+        {
+            img: "./assets/img/vinfast-data-01/Ludo_1624348528.webp",
+            name: "LUDO",
+            slogan: "DÁNG SÀNH ĐIỆU, KIỂU CHẤT CHƠI",
+            descs: ['Thiết kế sành điệu' , 
+                'Tính năng thông minh vượt trội' , 
+                'Vận hành êm ái, quãng đường di chuyển lên tới 70 km' ,
+                'Công nghệ pin Lithium bền bỉ, tiêu chuẩn chống nước IP57'
+            ]
+        },
+    ],
+    renderBlock3: function () {
+        var swiperSlideBlock3 = document.querySelector('.block3 .row');
+        const html3s = this.cars.map((car, index) => {            
+            return `
+                    <div class="l-3 m-6 c-12 swiper-slide">
+                        <div class="info-body-car info-body-header">
+                            <div class="dongxe">${car.dongxe}</div>
+                            <div class="slogan">${car.slogan}</div>
+                            <div class="name">${car.name}</div>
+                        </div>
+                        <img src="${car.logo}" alt="" class="swiper-lazy">
+                        <div class="info-body-car">
+                            <div class="desc">${car.descs[0]}</div>
+                            <div class="desc">${car.descs[1]}</div>
+                            <div class="desc">${car.descs[2]}</div>
+                            <div class="desc">${car.descs[3]}</div>  
+                        </div>
+                        <div class="lien_ket">
+                            Chi tiết
+                            <img class="swiper-lazy swiper-lazy-loaded" src="./assets/img/vinfast/logo_gray.svg" alt="">
+                        </div>
+                        <div>
+                            <img src="${car.img}" alt="" class="swiper-lazy">
+                        </div>
+                    </div>
+                `;
+        });
+        swiperSlideBlock3.innerHTML = html3s.join("");
+    },
+    renderBlock4 : function() {
+        var swiperSlideBlock4 = document.querySelector('.block4 .row');
+        const html4s = this.motorbikes.map((motorbike, index) => {
+            return `
+                    <div class="l-3 m-6 c-12 swiper-slide">
+                        <div class="img">
+                            <img src="${motorbike.img}" alt="" class="swiper-lazy">
+                        </div>
+                        <div class="info-motorbike">
+                            <div class="name">${motorbike.name}</div>
+                            <div class="slogan">${motorbike.slogan}</div>
+                            <div class="body">
+                                <div class="desc">${motorbike.descs[0]}</div>
+                                <div class="desc">${motorbike.descs[1]}</div>
+                                <div class="desc">${motorbike.descs[2]}</div>
+                                <div class="desc">${motorbike.descs[3]}</div>  
+                            </div>
+                        </div>
+                    </div>
+                `;
+        });
+        swiperSlideBlock4.innerHTML = html4s.join("");
+    }
+
+}
+
+app.renderBlock3();
+app.renderBlock4();
